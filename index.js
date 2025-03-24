0408df618b77519a5c9d3d79465d7e2a00b262a2
Quang Nguyen <wangnguenlc79@gmail.com>
2025-03-25 01:07:05 +0700

    feat: add pug

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
 const PORT = 3000;
 
+// Thiết lập views
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+// route + controller
 app.get("/", (req, res) => {
-	res.send("Trang chủ");
+	res.render("client/pages/home", {
+		pageTitle: "Trang chủ",
+	});
 });
 
 app.get("/tour", (req, res) => {
-	res.send("Trang tour");
+	res.render("client/pages/tour_list", {
+		pageTitle: "Danh sách tour",
+	});
 });
 
 app.listen(PORT, () => {

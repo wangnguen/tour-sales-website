5bb4cb8d936d648ba8172f1b7f32c629d6cd2e12
Quang Nguyen <wangnguenlc79@gmail.com>
2025-03-25 08:52:38 +0700

    feat: connect databases, change scripts in package.json

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+mongoose.connect(
+	"mongodb+srv://wangnguenlc79:Q12345%40%40@backend.ftdam.mongodb.net/tour_project_1",
+);
+
+const Tour = mongoose.model("Tour", { name: String, vehicle: String });
+
+// tours => Tour
+// users = User
+// products => Product
 
 const PORT = 3000;
+
+// Thiết lập thư mục chứa file tĩnh của Fontend:
+app.use(express.static(path.join(__dirname, "public")));
 
 // Thiết lập views
 app.set("view engine", "pug");
@@ -15,9 +28,13 @@ app.get("/", (req, res) => {
 	});
 });
 
-app.get("/tour", (req, res) => {
+app.get("/tours", async (req, res) => {
+	const tourList = await Tour.find({});
+
+	console.log(tourList);
 	res.render("client/pages/tour_list", {
 		pageTitle: "Danh sách tour",
+		tourList: tourList,
 	});
 });
 

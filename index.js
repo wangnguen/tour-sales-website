6483db5499d4c9e502a42008ac19d3d4c21eb902
Quang Nguyen <wangnguenlc79@gmail.com>
2025-03-28 02:46:33 +0700

    split database configuration

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-mongoose.connect(process.env.DATABASE);
 
 const PORT = process.env.PORT || 3000;
 const clientRoutes = require("./routes/client/index.route");
+const database = require("./configs/database");
 
 // Thiết lập thư mục chứa file tĩnh của Fontend:
 app.use(express.static(path.join(__dirname, "public")));
@@ -15,6 +13,9 @@ app.use(express.static(path.join(__dirname, "public")));
 // Thiết lập views
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
+
+// Kết nối db
+database.connect();
 
 // Thiết lập đường dẫn:
 app.use("/", clientRoutes);

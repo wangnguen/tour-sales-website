cbffb95536877ddc1171ac67117d8205e4b67c67
Quang Nguyen <wangnguenlc79@gmail.com>
2025-03-29 13:20:16 +0700

    feat: create tour detail page

--- controllers/client/tour.controller.js
+++ controllers/client/tour.controller.js
@@ -2,25 +2,17 @@ const Tour = require("../../models/tour.model");
 
 const list = async (req, res) => {
 	const tourList = await Tour.find({});
-	console.log(tourList);
+	// console.log(tourList);
 	res.render("client/pages/tour_list", {
 		titlePage: "Danh sách tour",
 		tourList: tourList,
 	});
 };
-const del = async (req, res) => {
-	const tourList = await Tour.find({});
-	res.render("client/pages/tour_list", {
-		titlePage: "Danh sách tour xoá",
-		tourList: tourList,
-	});
-};
-const create = async (req, res) => {
-	const tourList = await Tour.find({});
-	res.render("client/pages/tour_list", {
-		titlePage: "Danh sách tour tạo mới",
-		tourList: tourList,
+
+const detail = async (req, res) => {
+	res.render("client/pages/tour_detail", {
+		titlePage: "Chi tiết tour",
 	});
 };
 
-module.exports = { list, del, create };
+module.exports = { list, detail };
--- routes/client/tour.route.js
+++ routes/client/tour.route.js
@@ -3,7 +3,6 @@ const router = require("express").Router();
 const tourController = require("../../controllers/client/tour.controller");
 
 router.get("/", tourController.list);
-router.get("/del", tourController.del);
-router.get("/create", tourController.create);
+router.get("/detail", tourController.detail);
 
 module.exports = router;

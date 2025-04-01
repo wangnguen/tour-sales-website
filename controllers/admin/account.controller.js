4a1e51b760cde2d04c37786511cf76ae576fd9eb
Quang Nguyen <wangnguenlc79@gmail.com>
2025-04-01 16:09:23 +0700

    feat: Integrated password change page interface and otp code entry page

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -14,5 +14,21 @@ const forgotPassword = (req, res) => {
 		titlePage: "Quên mật khẩu",
 	});
 };
+const otpPassword = (req, res) => {
+	res.render("admin/pages/otp_password", {
+		titlePage: "Nhập mã OTP",
+	});
+};
+const resetPassword = (req, res) => {
+	res.render("admin/pages/reset_password", {
+		titlePage: "Đổi mật khẩu",
+	});
+};
 
-module.exports = { login, register, forgotPassword };
+module.exports = {
+	login,
+	register,
+	forgotPassword,
+	otpPassword,
+	resetPassword,
+};
--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -5,5 +5,7 @@ const accountController = require("../../controllers/admin/account.controller");
 router.get("/login", accountController.login);
 router.get("/register", accountController.register);
 router.get("/forgot_password", accountController.forgotPassword);
+router.get("/otp_password", accountController.otpPassword);
+router.get("/reset_password", accountController.resetPassword);
 
 module.exports = router;

0ee6c07cbc6d2743375cec132610dffbe8b10053
Quang Nguyen <wangnguenlc79@gmail.com>
2025-04-17 11:44:18 +0700

    feat: Select parent category when creating category

--- controllers/admin/category.controller.js
+++ controllers/admin/category.controller.js
@@ -1,13 +1,23 @@
 const Category = require("../../models/category.model");
+const categoryHelper = require("../../helpers/category.helper");
 
 const list = (req, res) => {
 	res.render("admin/pages/category_list", {
 		titlePage: "Quản lý danh mục",
 	});
 };
-const create = (req, res) => {
+const create = async (req, res) => {
+	const categoryList = await Category.find({
+		deleted: false,
+	});
+
+	const categoryTree = categoryHelper.buildCategoryTree(categoryList);
+
+	// console.log(categoryTree);
+
 	res.render("admin/pages/category_create", {
 		titlePage: "Tạo danh mục",
+		categoryList: categoryTree,
 	});
 };
 

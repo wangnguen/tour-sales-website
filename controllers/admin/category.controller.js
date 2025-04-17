92ea403135aa0ba7f85ea0086d44b7dd21d1a8de
Quang Nguyen <wangnguenlc79@gmail.com>
2025-04-18 02:23:30 +0700

    feat: make category management page

--- controllers/admin/category.controller.js
+++ controllers/admin/category.controller.js
@@ -1,9 +1,37 @@
+const moment = require("moment");
+
 const Category = require("../../models/category.model");
+const AccountAdmin = require("../../models/admin_account.model");
 const categoryHelper = require("../../helpers/category.helper");
 
-const list = (req, res) => {
+const list = async (req, res) => {
+	const categoryList = await Category.find({
+		deleted: "false",
+	}).sort({
+		position: "desc",
+	});
+
+	for (const item of categoryList) {
+		if (item.createdBy) {
+			const infoAccountCreated = await AccountAdmin.findOne({
+				_id: item.createdBy,
+			});
+			item.createdByFullName = infoAccountCreated.fullName;
+		}
+
+		if (item.updatedBy) {
+			const infoAccountUpdated = await AccountAdmin.findOne({
+				_id: item.updatedBy,
+			});
+			item.updatedByFullName = infoAccountUpdated.fullName;
+		}
+		item.createdAtFormat = moment(item.createdAt).format("HH:mm - DD/MM/YYYY");
+		item.updatedAtFormat = moment(item.updatedAt).format("HH:mm - DD/MM/YYYY");
+	}
+
 	res.render("admin/pages/category_list", {
 		titlePage: "Quản lý danh mục",
+		categoryList: categoryList,
 	});
 };
 const create = async (req, res) => {

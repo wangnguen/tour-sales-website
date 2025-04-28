fea2bc5060393395a6df031008fa8b5f0164e707
Quang Nguyen <wangnguenlc79@gmail.com>
2025-04-28 16:48:42 +0700

    feat: Tour list

--- controllers/admin/tour.controller.js
+++ controllers/admin/tour.controller.js
@@ -1,13 +1,47 @@
+const moment = require("moment");
+
 const Category = require("../../models/category.model");
 const City = require("../../models/city.model");
 const Tour = require("../../models/tour.model");
+
+const AccountAdmin = require("../../models/admin_account.model");
+
 const categoryHelper = require("../../helpers/category.helper");
 
-const list = (req, res) => {
+const list = async (req, res) => {
+	const find = {
+		deleted: false,
+	};
+
+	const tourList = await Tour.find(find).sort({
+		position: "desc",
+	});
+
+	for (const item of tourList) {
+		if (item.createdBy) {
+			const infoAccountCreated = await AccountAdmin.findOne({
+				_id: item.createdBy,
+			});
+			item.createdByFullName = infoAccountCreated.fullName;
+		}
+
+		if (item.updatedBy) {
+			const infoAccountUpdated = await AccountAdmin.findOne({
+				_id: item.updatedBy,
+			});
+			item.updatedByFullName = infoAccountUpdated.fullName;
+		}
+
+		item.createdAtFormat = moment(item.createdAt).format("HH:mm - DD/MM/YYYY");
+		item.updatedAtFormat = moment(item.updatedAt).format("HH:mm - DD/MM/YYYY");
+	}
+
 	res.render("admin/pages/tour_list", {
 		titlePage: "Quản lý tour",
+		tourList: tourList,
 	});
 };
+
 const create = async (req, res) => {
 	const categoryList = await Category.find({
 		deleted: false,

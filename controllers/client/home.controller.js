41cfe8e36637f710b2e3ceaa3186c499911da088
Quang Nguyen <wangnguenlc79@gmail.com>
2025-05-11 12:51:44 +0700

    feat: display data to section2

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,25 @@
-const homeController = (req, res) => {
+const moment = require("moment");
+
+const Tour = require("../../models/tour.model");
+
+const homeController = async (req, res) => {
+	// Section 2
+	const tourListSection2 = await Tour.find({
+		deleted: false,
+		status: "active",
+	})
+		.sort({
+			position: "desc",
+		})
+		.limit(6);
+	for (const item of tourListSection2) {
+		item.departureDateFormat = moment(item.departureDate).format("DD/MM/YYYY");
+	}
+	// End section 2
+
 	res.render("client/pages/home", {
 		titlePage: "Trang chủ",
+		tourListSection2: tourListSection2,
 	});
 };
 module.exports = { homeController };

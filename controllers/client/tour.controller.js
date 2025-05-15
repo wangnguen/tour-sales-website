cace09e89a99de8c96b899282b2df854bfa72417
Quang Nguyen <wangnguenlc79@gmail.com>
2025-05-15 16:53:24 +0700

    feat: breadscrumb client

--- controllers/client/tour.controller.js
+++ controllers/client/tour.controller.js
@@ -1,9 +1,74 @@
 const Tour = require("../../models/tour.model");
 
+const Category = require("../../models/category.model");
+
 const detail = async (req, res) => {
-	res.render("client/pages/tour_detail", {
-		titlePage: "Chi tiết tour",
+	const slug = req.params.slug;
+
+	// Tìm tour theo slug
+	const tourDetail = await Tour.findOne({
+		slug: slug,
+		status: "active",
+		deleted: false,
 	});
+	// Hết tìm tour theo slug
+
+	if (tourDetail) {
+		// Breadcrumb
+		var breadcrumb = {
+			image: tourDetail.avatar,
+			title: tourDetail.name,
+			list: [
+				{
+					link: "/",
+					title: "Trang chủ",
+				},
+			],
+		};
+
+		const category = await Category.findOne({
+			_id: tourDetail.category,
+			deleted: false,
+			status: "active",
+		});
+
+		if (category) {
+			// Tìm danh mục cha
+			if (category.parent) {
+				const parentCategory = await Category.findOne({
+					_id: category.parent,
+					deleted: false,
+					status: "active",
+				});
+
+				if (parentCategory) {
+					breadcrumb.list.push({
+						link: `/category/${parentCategory.slug}`,
+						title: parentCategory.name,
+					});
+				}
+			}
+
+			// Thêm danh mục hiện tại
+			breadcrumb.list.push({
+				link: `/category/${category.slug}`,
+				title: category.name,
+			});
+		}
+
+		breadcrumb.list.push({
+			link: `/tour/detail/${slug}`,
+			title: tourDetail.name,
+		});
+		// End breadcrumb
+
+		res.render("client/pages/tour_detail", {
+			titlePage: "Chi tiết tour",
+			breadcrumb: breadcrumb,
+		});
+	} else {
+		res.redirect("/");
+	}
 };
 
 module.exports = { detail };
--- routes/client/tour.route.js
+++ routes/client/tour.route.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 
 const tourController = require("../../controllers/client/tour.controller");
 
-router.get("/detail", tourController.detail);
+router.get("/detail/:slug", tourController.detail);
 
 module.exports = router;

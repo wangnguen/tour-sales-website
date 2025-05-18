c77492d3a3ca6dc75c9a50fe698eb004a0b74e4d
Quang Nguyen <wangnguenlc79@gmail.com>
2025-05-18 17:06:26 +0700

    feat: Tour quantity update

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -650,7 +650,13 @@ const drawCart = () => {
                 <div class="inner-item">
                   <div class="inner-item-label">Người lớn:</div>
                   <div class="inner-item-input">
-                    <input value="${item.quantityAdult}" min="0" type="number">
+                    <input 
+											value="${item.quantityAdult}"
+											min="0"
+											type="number"
+											input-quantity="quantityAdult"
+											tour-id="${item.tourId}"
+										>
                   </div>
                   <div class="inner-item-price">
                     <span>${item.quantityAdult}</span>
@@ -663,9 +669,13 @@ const drawCart = () => {
                 <div class="inner-item">
                   <div class="inner-item-label">Trẻ em:</div>
                   <div class="inner-item-input">
-                    <input value="${
-											item.quantityChildren
-										}" min="0" type="number">
+                    <input 
+											value="${item.quantityChildren}" 
+											min="0" 
+											type="number"
+											input-quantity="quantityChildren"
+											tour-id="${item.tourId}"
+										>
                   </div>
                   <div class="inner-item-price">
                     <span>${item.quantityChildren}</span>
@@ -678,7 +688,13 @@ const drawCart = () => {
                 <div class="inner-item">
                   <div class="inner-item-label">Em bé:</div>
                   <div class="inner-item-input">
-                    <input value="${item.quantityBaby}" min="0" type="number">
+                    <input 
+											value="${item.quantityBaby}"
+											min="0"
+											type="number"
+											input-quantity="quantityBaby"
+											tour-id="${item.tourId}"
+										>
                   </div>
                   <div class="inner-item-price">
                     <span>${item.quantityBaby}</span>
@@ -721,6 +737,23 @@ const drawCart = () => {
 				const cartTotal = document.querySelector("[cart-total]");
 				cartTotal.innerHTML = totalPrice.toLocaleString("vi-VN");
 				// Hết Tính tổng tiền
+
+				// Sự kiện cập nhật số lượng
+				const listInputQuantity = document.querySelectorAll("[input-quantity]");
+				listInputQuantity.forEach((input) => {
+					input.addEventListener("change", () => {
+						const tourId = input.getAttribute("tour-id");
+						const name = input.getAttribute("input-quantity");
+						const quantity = parseInt(input.value);
+
+						const cart = JSON.parse(localStorage.getItem("cart"));
+						const itemUpdate = cart.find((item) => item.tourId === tourId);
+						itemUpdate[name] = quantity;
+						localStorage.setItem("cart", JSON.stringify(cart));
+						drawCart();
+					});
+				});
+				// Hết sự kiện cập nhật số lượng
 			}
 		});
 };

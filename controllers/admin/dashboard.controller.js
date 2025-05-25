15b1a093b36ff005444f496911c1bcd5577107ca
Quang Nguyen <wangnguenlc79@gmail.com>
2025-05-26 02:48:10 +0700

    feat: Overview block

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,10 +1,11 @@
 const moment = require('moment');
 
 const Order = require('../../models/order.model');
-
+const AccountAdmin = require('../../models/admin_account.model');
 const variableConfig = require('../../configs/variable');
 
 const dashboard = async (req, res) => {
+  // Section 3
   const find = {
     deleted: false
   };
@@ -29,10 +30,31 @@ const dashboard = async (req, res) => {
     orderDetail.createdAtTime = moment(orderDetail.createdAt).format('HH:mm');
     orderDetail.createdAtDate = moment(orderDetail.createdAt).format('DD/MM/YYYY');
   }
+  // End section 3
+
+  // Section 1
+  const overView = {
+    totalAdmin: 0,
+    totalUser: 0,
+    totalOrder: 0,
+    totalPrice: 0
+  };
+
+  overView.totalAdmin = await AccountAdmin.countDocuments({
+    deleted: false
+  });
+
+  overView.totalOrder = orderList.length;
+
+  overView.totalPrice = orderList.reduce((sum, item) => {
+    return sum + item.total;
+  }, 0);
+  // End section 1
 
   res.render('admin/pages/dashboard', {
     titlePage: 'Trang tổng quan',
-    orderList
+    orderList,
+    overView
   });
 };
 

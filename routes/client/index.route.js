152148437096f7ffdc826159f1cdf8632df9fa4d
Quang Nguyen <wangnguenlc79@gmail.com>
2025-05-30 23:58:04 +0700

    feat: add page 404 client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,24 +1,30 @@
-const router = require("express").Router();
-const tourRoutes = require("./tour.route");
-const homeRoutes = require("./home.route");
-const cartRoutes = require("./cart.route");
-const contactRoutes = require("./contact.route");
-const categoryRoutes = require("./category.route");
-const searchRoutes = require("./search.route");
-const orderRoutes = require("./order.route");
+const router = require('express').Router();
+const tourRoutes = require('./tour.route');
+const homeRoutes = require('./home.route');
+const cartRoutes = require('./cart.route');
+const contactRoutes = require('./contact.route');
+const categoryRoutes = require('./category.route');
+const searchRoutes = require('./search.route');
+const orderRoutes = require('./order.route');
 
-const settingMiddleware = require("../../middlewares/client/setting.middleware");
-const categoryMiddleware = require("../../middlewares/client/category.middleware");
+const settingMiddleware = require('../../middlewares/client/setting.middleware');
+const categoryMiddleware = require('../../middlewares/client/category.middleware');
 
 router.use(settingMiddleware.websiteInfo);
 router.use(categoryMiddleware.list);
 
-router.use("/", homeRoutes);
-router.use("/tour", tourRoutes);
-router.use("/cart", cartRoutes);
-router.use("/contact", contactRoutes);
-router.use("/category", categoryRoutes);
-router.use("/search", searchRoutes);
-router.use("/order", orderRoutes);
+router.use('/', homeRoutes);
+router.use('/tour', tourRoutes);
+router.use('/cart', cartRoutes);
+router.use('/contact', contactRoutes);
+router.use('/category', categoryRoutes);
+router.use('/search', searchRoutes);
+router.use('/order', orderRoutes);
+
+router.get('*', (req, res) => {
+  res.render('client/pages/error_404.pug', {
+    titlePage: '404 Not Found'
+  });
+});
 
 module.exports = router;
